030ba9128e33426e56d12efcacaac029190ba5be
Warwick New <warwick.l.e.new@gmail.com>
2018-02-16 11:56:27 +0000

    more efficient file readin

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -17,12 +17,12 @@ func _ready():
 			grid[x].append(0)
 			set_cell(x,y,3)
 			
-			#test new file reading
-			if file.file_exists("res://Game/Resources/Levels/level0-0.txt"):
-				file.open("res://Game/Resources/Levels/level0-0.txt", file.READ)
-				textMap = file.get_as_text()
-				file.close()
-			print (textMap)
+	#test new file reading
+	if file.file_exists("res://Game/Resources/Levels/level0-0.txt"):
+		file.open("res://Game/Resources/Levels/level0-0.txt", file.READ)
+		textMap = file.get_as_text()
+		file.close()
+	print (textMap)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.

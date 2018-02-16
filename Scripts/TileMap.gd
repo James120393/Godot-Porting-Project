764ad9a2574f692b845894e9669e2caeaf2a48e8
Warwick New <warwick.l.e.new@gmail.com>
2018-02-16 10:46:45 +0000

    Discovered method of latering tilemap.

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -1,10 +1,23 @@
 extends TileMap
 
+var width = 20
+var height = 10
+var grid = []
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	for x in range(0, width):
+		grid.append([])
+		for y in range(0, height):
+			grid[x].append(0)
+			print (self.get_cell(x,y))
+			set_cell(x,y,1)
+	#print (grid)
+	#print(self.get_used_cells())
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass

e577e9c37b19be2728bc30b7cf7d2968247a62f2
James120393 <james_hellman@hotmail.co.uk>
2018-05-03 14:49:16 +0100

    Added more button images Added controller instructions

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -9,7 +9,7 @@ func _ready():
 	set_process_input(true)
 	
 func _input(event):
-	if get_node("InfoBG/Cross").is_pressed():
+	if get_node("InfoBG/Cross").is_pressed() or get_node("ControlsBG/Cross").is_pressed():
 		_on_cross_Button_pressed()
 	if get_node("LevelSelectBG/Cross").is_pressed():
 		_on_cross_Button_pressed()
@@ -17,6 +17,10 @@ func _input(event):
 		_on_info_Button_pressed()
 	if get_node("Play").is_pressed():
 		_on_play_Button_pressed()
+	if get_node("Controls").is_pressed():
+		_on_controls_Button_pressed()
+	if get_node("Quit").is_pressed():
+		_on_quit_Button_pressed()
 
 func _on_info_Button_pressed():
 	get_node("InfoBG").set_visible(true)
@@ -26,10 +30,18 @@ func _on_cross_Button_pressed():
 	get_node("InfoBG").set_visible(false)
 	get_node("LevelSelectBG").set_visible(false)
 	get_node("Play").set_disabled(false)
+	get_node("ControlsBG").set_visible(false)
 
 func _on_play_Button_pressed():
 	get_node("LevelSelectBG").set_visible(true)
-	
+
+func _on_controls_Button_pressed():
+	get_node("ControlsBG").set_visible(true)
+	get_node("Play").set_disabled(true)
+
+func _on_quit_Button_pressed():
+	get_tree().quit()
+
 func load_Level(level_Number):
 	var level = str("res://Scenes/Level_" + level_Number + ".tscn")
 	get_tree().change_scene(level)
